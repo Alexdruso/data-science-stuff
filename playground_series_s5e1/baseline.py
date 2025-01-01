63a938292b535fb01eb28b539062b66ab16fb29f
Alessandro Sanvito <alessandro.sanvito@gmail.com>
2025-01-01 13:21:53 +0100

    chore: better toml

--- playground_series_s5e1/baseline.py
+++ playground_series_s5e1/baseline.py
@@ -1,10 +1,11 @@
-import polars as pl
+import pandas as pd
 import argparse
+from pycaret.time_series import setup
 
 def main(data_path: str) -> None:
-    train_set: pl.DataFrame = pl.read_csv(source=f"{data_path}/train.csv")
-
+    train_set: pd.DataFrame = pd.read_csv(source=f"{data_path}/train.csv")
     print(train_set)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
