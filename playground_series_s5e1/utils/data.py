5ad3a3042369c9c60e3c0b31146c8a3108a46cfa
Alessandro Sanvito <alessandro.sanvito@gmail.com>
2025-01-01 20:53:15 +0100

    feat: load data

--- playground_series_s5e1/utils/data.py
+++ playground_series_s5e1/utils/data.py
@@ -2,18 +2,18 @@ from pathlib import Path
 import pandas as pd
 import polars as pl
 
-
 def load_data(
     path: str | Path,
     *,
-    train_set: bool = True,
+    multi_target: bool,
+    train_set: bool,
 ) -> pd.DataFrame:
     path: Path = Path(path)
     file_name: str = "train.csv" if train_set else "test.csv"
     path = path / file_name
 
     data: pl.DataFrame = pl.read_csv(
-        file_name,
+        source=path,
         try_parse_dates=True,
         schema_overrides={
             "country": pl.Categorical,
@@ -21,5 +21,21 @@ def load_data(
             "product": pl.Categorical,
         },
     )
+
+    if multi_target:
+        data = data.with_columns(
+            (data["country"] + "_" + data["store"] + "_" + data["product"]).alias("identifier")
+        )
+
+        # Drop the original columns before pivoting
+        data = data.drop(["country", "store", "product"])
+
+        # Pivot the data to merge rows with the same date
+        data = data.pivot(
+            values=[col for col in data.columns if col not in ["date", "identifier"]],
+            index=["date"],
+            columns="identifier",
+            aggregate_function=None
+        )
 
     return data.to_pandas()
