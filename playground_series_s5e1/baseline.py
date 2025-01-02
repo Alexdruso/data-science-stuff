76b5811e663aefcd16a2c1840dc7d407711031fe
Alessandro Sanvito <alessandro.sanvito@gmail.com>
2025-01-02 12:02:27 +0100

    feat: baseline

--- playground_series_s5e1/baseline.py
+++ playground_series_s5e1/baseline.py
@@ -1,20 +1,35 @@
 import pandas as pd
 import argparse
 from pycaret.time_series import setup, compare_models
+from utils.data import load_data
 
 
 def main(data_path: str) -> None:
-    train_set: pd.DataFrame = pd.read_csv(filepath_or_buffer=f"{data_path}/train.csv")
-    train_set["date"] = pd.to_datetime(train_set["date"])
-    print(train_set)
-    print(train_set.dtypes)
-    train_set = train_set.dropna(subset=["num_sold"])
-
-    experiment = setup(
-        data=train_set, target="num_sold", index="date", ignore_features=["id"]
+    train_set: pd.DataFrame = load_data(
+        path=data_path, multi_target=False, train_set=True
     )
 
-    best_model = compare_models()
+    countries: set[str] = set(train_set["country"])
+    stores: set[str] = set(train_set["store"])
+    products: set[str] = set(train_set["product"])
+
+    train_set = train_set.set_index(keys=["country", "store", "product"])
+
+    print(f"{train_set}")
+
+    for country in countries:
+        for store in stores:
+            for product in products:
+                print(f"Learning to predict: {country=} {store=} {product=}")
+
+                experiment = setup(
+                    data=train_set.loc[(country, store, product)],
+                    target="num_sold",
+                    index="date",
+                    ignore_features=["id"],
+                )
+
+                best_model = compare_models()
 
 
 if __name__ == "__main__":
