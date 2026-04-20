51d0a8930688490f920587c22d796433387653a2
alexdruso <alessandro.sanvito@gmail.com>
2026-04-20 22:05:53 +0200

    fix

--- data_science_stuff/_version.py
+++ data_science_stuff/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev58+g392bedcd1.d20260418'
-__version_tuple__ = version_tuple = (0, 1, 'dev58', 'g392bedcd1.d20260418')
+__version__ = version = '0.1.dev61+gcec097663.d20260420'
+__version_tuple__ = version_tuple = (0, 1, 'dev61', 'gcec097663.d20260420')
 
-__commit_id__ = commit_id = 'g392bedcd1'
+__commit_id__ = commit_id = 'gcec097663'
